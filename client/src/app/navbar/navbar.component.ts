5d9a905fe9133d4d34735e4f7ab55a034e3a9c2d
ali <a.aloreabi2000@gmail.com>
2024-03-26 18:57:13 -0400

    fixed component routing

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { AuthService } from '../auth.service';
+import { RouterLink } from '@angular/router';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, NgIf],
+  imports: [CommonModule, MatToolbarModule, NgIf, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
